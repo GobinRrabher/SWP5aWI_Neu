7b1e344969a3516e602dd88f5a283cc9720bdc70
GobinRRabher <robin.grabher@student.htldornbirn.at>
2022-11-29 14:22:07 +0100

    29.11 anfang

--- car/src/figures/Main.java
+++ car/src/figures/Main.java
@@ -2,6 +2,6 @@ package figures;
 
 public class Main {
     public static void main(String[] args) {
-        Figure r1 = new Rectangle("rectangle1");
+        Figure r1 = new Rectangle("rectangle1", 5, 4);
     }
 }
--- car/src/figures/Rectangle.java
+++ car/src/figures/Rectangle.java
@@ -2,7 +2,13 @@ package figures;
 
 public class Rectangle extends Figure{
 
-    public Rectangle(String name){
+    private int length;
+    private int height;
+    public Rectangle(String name, int length, int height){
         super(name);
+        this.length = length;
+        this.height = height;
     }
+
+
 }
